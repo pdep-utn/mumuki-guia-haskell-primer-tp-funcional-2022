44fd7451ff63739c2d71659a39e7fafec4a61af5
licfleischman.af@gmail.com <licfleischman.af@gmail.com>
2022-04-23 13:29:10 +0000

    Mumuki Export on 2022-04-23 13:29:10 +0000

--- 00004_PROGRAMAR CON DIV, STR Y LOD/test.hs
+++ 00004_PROGRAMAR CON DIV, STR Y LOD/test.hs
@@ -1,4 +1,22 @@
-describe "Descripción del grupo" $ do 
-  
-  it "Descripción del ejemplo" $ do
-    True `shouldBe` True
+ describe "Test Punto 4 - Tests de programa División" $ do
+        it "STR 2 5 para la memoria" $ do
+            ((memoria . str 2 5) at8086) `shouldBe` [1, 5, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+        it "LOD 2 de una memoria vacía debe dar 0" $ do
+            (acumuladorA $ lod 2 xt8088) `shouldBe` 0
+
+        it "Division por cero da error" $ do
+            (mensajeError $ divisionPorCero xt8088) `shouldBe` "DIVISION BY ZERO"
+    
+        it "Division por cero aumenta program counter" $ do
+            (programCounter $ divisionPorCero xt8088) `shouldBe` 6
+            
+        it "Division de 12 por 4 se resuelve bien en Acumulador A" $ do
+            (acumuladorA $ division12Por4 xt8088) `shouldBe` 3
+
+        it "Division de 12 por 4 blanquea Acumulador B" $ do
+            (acumuladorB $ division12Por4 xt8088) `shouldBe` 0
+
+        it "Division de 12 por 4 no deja el mensaje de error porque funciona bien" $ do
+            (mensajeError $ division12Por4 xt8088) `shouldBe` ""
+        
